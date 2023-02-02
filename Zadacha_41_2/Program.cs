80d8fe8d5de1c6b34747cbfea376403e80dd3c62
Arteeex <arteeex187@gmail.com>
2023-02-02 17:37:32 +0700

    Zadacha_41_2_variant

---
+++ Zadacha_41_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.Write("How many numbers do we enter? -> ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] arrayNumbers = new int [N];
+int count = 0;
+
+for (int i = 0; i < arrayNumbers.Length;i++ )
+{
+    arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+foreach (var item in arrayNumbers)
+{
+    Console.Write(item + " ");
+}
+for (int i = 0; i < arrayNumbers.Length; i++)
+{
+    if (arrayNumbers[i] > 0)
+    {
+    count++;
+    }
+}
+Console.WriteLine("\nThe number of numbers greater than zero: " +count);
