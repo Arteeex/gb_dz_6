83aff7349cd596c62e56a2b45dd4fb44415789ef
Arteeex <arteeex187@gmail.com>
2023-02-02 17:35:21 +0700

    zadacha_41_1_variant

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите числа, через пробел: ");
+string value = Console.ReadLine();
+string[] temp = value.Split(new Char[] {' '});
+int count = 0;
+foreach(string item in temp)
+{
+    int num = int.Parse(item);
+    
+    if(num > 0)
+    {
+        count++;
+    }
+
+}
+Console.WriteLine("\nЧисел больше нуля: " +count);
